74e8a15bf9a502f7fc73ecde8b93edc581369657
declan conaghan <deccy86@gmail.com>
2020-08-13 01:46:32 +0100

    Base syntax module complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Dec', age: 24},
+      {name: 'Steph', age: 26}
+    ]
+  }
+
+  switchNameHandler = newName => {
+    this.setState({
+      persons: [
+        {name: newName, age: 28},
+        {name: 'Dec', age: 24},
+        {name: 'Steph', age: 27}
+      ]
+    })
+  }
+
+  nameChangedHandler = event => {
+    this.setState({
+      persons: [
+        {name: 'Max', age: 28},
+        {name: event.target.value, age: 24},
+        {name: 'Steph', age: 26}
+      ]
+    })
+  }
+
   render() {
+    const style={
+      backgroundColour: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button style={style} onClick={() => this.switchNameHandler('Dec!')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Dec!!!!')}
+          changed={this.nameChangedHandler}>I love coding!</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
     );
   }
